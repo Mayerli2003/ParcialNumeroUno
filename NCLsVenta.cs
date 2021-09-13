ae3616c795808794203848e6609e5c88fbe17fe1
Mayerli2003 <87994175+Mayerli2003@users.noreply.github.com>
2021-09-13 15:39:18 -0600

    commit avance

---
+++ NCLsVenta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrimerParcial.Entidades;
+
+namespace PrimerParcial.Negocio
+{
+    class NCLsVenta
+    {
+        ClsVentas ventas = new ClsVentas();
+        
+        ClsVentas  cobro(ClsVentas ventas)
+        {
+            ClsVentas producto1 = new ClsVentas(1,"churro", "Marca Diana", 00.50);
+            ClsVentas producto2 = new ClsVentas(2, "Pan", "lilian", 1.50);
+            ClsVentas producto3 = new ClsVentas(3, "agua ", "Cristal", 00.50);
+            ClsVentas producto4 = new ClsVentas(4, "leche", "la vaquita", 1.00);
+            Console.WriteLine("Ingrese el id del producto");
+            Console.WriteLine("Productos a la venta" +
+               "\n1 - Churro" +
+               "\n2 - Pan" +
+               "\n3 - Agua " +
+               "\n4 - Lwche");
+            ventas.Idproducto = Console.ReadLine();
+            if (ventas)
+            {
+                double Respuesta;
+                Respuesta = ventas.PrecioProducto * ventas.CantidadProducto;
+                return Respuesta;
+            }
+            
+        }
+    }
+}
---
+++ PrimerParcial/ClsVentas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerParcial.Entidades
+{
+    class ClsVentas
+    {
+        private int idproducto;
+        private string nombreProducto;
+        private string descripcionProducto;
+        private double precioProducto;
+        private int cantidadProducto;
+
+        public ClsVentas() { }
+        public ClsVentas(int idproducto, string nombreProducto, string descripcionProducto, double precioProducto, int cantidadProducto)
+        {
+            this.idproducto = idproducto;
+            this.nombreProducto = nombreProducto;
+            this.descripcionProducto = descripcionProducto;
+            this.precioProducto = precioProducto;
+            this.cantidadProducto = cantidadProducto;
+        }
+
+        public int Idproducto { get => idproducto; set => idproducto = value; }
+        public string NombreProducto { get => nombreProducto; set => nombreProducto = value; }
+        public string DescripcionProducto { get => descripcionProducto; set => descripcionProducto = value; }
+        public double PrecioProducto { get => precioProducto; set => precioProducto = value; }
+        public int CantidadProducto { get => cantidadProducto; set => cantidadProducto = value; }
+    }
+}
